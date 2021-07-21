d4c7256bfa853e244eeb1dd10f29c40324c0b726
Atryom <238136@mail.ru>
2021-07-21 23:19:00 +0300

    lesson 2 witout homework

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ const getElement = (tagName, classNames, attributes) => {
 		}
 	}
 	return element;
-}
+};
 
 const createHeader = (param) => {
 	const header = getElement('header');
@@ -53,11 +53,11 @@ const createHeader = (param) => {
 		const menu = getElement('nav', ['menu-list']);
 		for (let elem of param.header.menu) {
 			const a = getElement('a', ['menu-link'], {
-				href: elem.link
+				href: elem.link,
+				textContent: elem.title,
 			});
-			a.textContent = elem.title;
 			menu.append(a);
-			wrapper.append(menu)
+			wrapper.append(menu);
 		}
 	}
 
@@ -85,23 +85,97 @@ const createHeader = (param) => {
 	return header;
 };
 
+const createMain = ({
+	title,
+	main: {genre, rating, description,	trailer}}) => {
+
+		const main = getElement('main');
+		const container = getElement('div', ['container']);
+		const wrapper = getElement('div', ['main-content']);
+		const content = getElement('div', ['content']);
+
+		if (genre) {
+			const span = getElement('span', ['genre', 'animated', 'fadeInRight'], {
+				textContent: genre,
+			});
+			content.append(span);
+		}
+
+		if (rating) {
+			const ratingBlock = getElement('div', ['rating', 'rating', 'fadeInRight']);
+			const ratingStars = getElement('div', ['rating-stars']);
+			const ratingNumber = getElement('div', ['rating-number'], {
+				textContent: `${rating}/10`,
+			});
+
+			for (let i = 0; i < 10; i++) {
+				const star = getElement('img', ['star'], {
+					alt: i ? '' : `Рейтинг ${rating} из 10`,
+					src: i < rating ? 'img/star.svg' : 'img/star-o.svg'
+				});
+				ratingStars.append(star);
+			}
+
+			ratingBlock.append(ratingStars, ratingNumber);
+			content.append(ratingBlock);
+		}
+
+		if (title) {
+			const h1 = getElement('h1', ['main-title', 'animated', 'fadeInRight'], {
+				textContent: title,
+			});
+			content.append(h1);
+		}
+
+		if (description) {
+			const p = getElement('p', ['main-description', 'animated', 'fadeInRight'], {
+				textContent: description,
+			});
+			content.append(p);
+		}
+
+		if (trailer) {
+			const link = getElement('a', ['button', 'animated', 'fadeInRight'], {
+				href: trailer,
+				textContent: 'Смотреть трейлер',
+			});
+			content.append(link);
+		}
+		main.append(container);
+		container.append(wrapper);
+		wrapper.append(content);
+
+		return main;
+};
+
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
+	app.classList.add('body-app');
 
 	if (options.title) {
 		document.querySelector('title').textContent = options.title;
 	}
 
-	app.classList.add('body-app');
+	if (options.background) {
+		app.style.backgroundImage = options.background ?
+			`url("${options.background}")`:
+			'';
+	}
+
 
 	if (options.header) {
 		app.append(createHeader(options));
+	}
+
+	if (options.main) {
+		app.append(createMain(options));
 	}
 };
 
 
 movieConstructor('.app', {
 	title: 'Ведьмак',
+	background: 'witcher/background.jpg',
 	header: {
 		logo: 'witcher/logo.png',
 		social: [
@@ -135,5 +209,11 @@ movieConstructor('.app', {
 				link: '#',
 			},
 		]
-	}
+	},
+	main: {
+		genre: '2019,фэнтези',
+		rating: '8',
+		description: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой	мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
+		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+	},
 });
